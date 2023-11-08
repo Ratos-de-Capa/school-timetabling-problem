2f78444c000d10665f2c2efc5070611eed9d53c7
1canas <pedro.s.furlaneto@gmail.com>
2023-11-08 19:26:52 -0300

    feat(set de conflitos): função que define as arestas e gera a matriz de adjacencia

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
 import json
 
 
-def get_teacher(row):
-    teacher = ""
-    for col in df.columns[6:]:
-        if row[col] == "X":
-            teacher = col
-    return teacher
-
-def get_cenario(path):
-    df = pd.read_csv(path) # path example csv/cenario-2semestre.csv
-
-    # Aplica a função para criar a coluna "prof"
-    df['prof'] = df.apply(get_teacher, axis=1)
-
-    # Seleciona apenas as colunas desejadas
-    df = df[['Curso', 'Semestre', 'Código', 'Disciplina', 'CH', 'prof']]
-
-    # Converte o DataFrame para uma lista de dicionários
-    items = df.to_dict(orient='records')
-
-    # Exibe os itens
-    # for item in items:
-    #     print(item)
-        
-    print(items)
-    
-
-
 class Conflito:
   codigo: str
   type: str | int
@@ -54,6 +27,36 @@ class Vertice:
         self.conflitos: list[Conflito] = []
 
 
+def get_teacher(row):
+    teacher = ""
+    for col in df.columns[6:]:
+        if row[col] == "X":
+            teacher = col
+    return teacher
+
+def get_cenario(path):
+    df = pd.read_csv(path) # path example csv/cenario-2semestre.csv
+
+    # Aplica a função para criar a coluna "prof"
+    df['prof'] = df.apply(get_teacher, axis=1)
+
+    # Seleciona apenas as colunas desejadas
+    df = df[['Curso', 'Semestre', 'Código', 'Disciplina', 'CH', 'prof']]
+
+    # Converte o DataFrame para uma lista de dicionários
+    items = df.to_dict(orient='records')
+
+    # Exibe os itens
+    # for item in items:
+    #     print(item)
+        
+    print(items)
+    
+
+def read_json_data(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
 
 def color_vertex(v1: Vertice, v2: Vertice):
     points = 0
