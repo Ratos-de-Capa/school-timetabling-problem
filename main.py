979dfb7f94fbe04c526f3ddc892e65ccdc0efa23
1canas <pedro.s.furlaneto@gmail.com>
2023-11-08 19:48:44 -0300

    feat(algoritmo de coloracao): algoritmo guloso

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import json
 
 
 class Conflito:
-  codigo: str
-  type: str | int
+    codigo: str
+    type: str | int
 
-class Disciplina:    
-  curso: str
-  semestre: str
-  codigo: str
-  nome: str
-  ch: str
-  prof: str
-  conflitos: list[Conflito]
+
+class Disciplina:
+    curso: str
+    semestre: str
+    codigo: str
+    nome: str
+    ch: str
+    prof: str
+    conflitos: list[Conflito]
 
 
 class Vertice:
@@ -23,7 +24,7 @@ class Vertice:
         self.disciplina: str = disciplina
         self.ch: str = ch
         self.prof: str = prof
-        
+
         self.conflitos: list[Conflito] = []
 
 
@@ -34,27 +35,28 @@ def get_teacher(row):
             teacher = col
     return teacher
 
+
 def get_cenario(path):
-    df = pd.read_csv(path) # path example csv/cenario-2semestre.csv
+    df = pd.read_csv(path)  # path example csv/cenario-2semestre.csv
 
     # Aplica a função para criar a coluna "prof"
-    df['prof'] = df.apply(get_teacher, axis=1)
+    df["prof"] = df.apply(get_teacher, axis=1)
 
     # Seleciona apenas as colunas desejadas
-    df = df[['Curso', 'Semestre', 'Código', 'Disciplina', 'CH', 'prof']]
+    df = df[["Curso", "Semestre", "Código", "Disciplina", "CH", "prof"]]
 
     # Converte o DataFrame para uma lista de dicionários
-    items = df.to_dict(orient='records')
+    items = df.to_dict(orient="records")
 
     # Exibe os itens
     # for item in items:
     #     print(item)
-        
+
     print(items)
-    
+
 
 def read_json_data(filename):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         data = json.load(file)
     return data
 
@@ -63,9 +65,8 @@ def generate_adj_matrix(cenario):
     matrix = []
 
     for v1 in cenario:
-        print(v1)
         adj = []
-        
+
         for v2 in cenario:
             adj.append(generate_score(v1, v2))
 
@@ -74,26 +75,60 @@ def generate_adj_matrix(cenario):
     return matrix
 
 
+def coloracao_por_matriz_adjacencia(matriz_adjacencia):
+    num_vertices = len(matriz_adjacencia)
+    cores = [-1] * num_vertices  # Inicializa todas as cores como -1
+
+    def eh_cor_segura(vertice, cor):
+        for i in range(num_vertices):
+            if matriz_adjacencia[vertice][i] == 1 and cores[i] == cor:
+                return False
+        return True
+
+    def colorir_util(vertice, num_cores):
+        if vertice == num_vertices:
+            return True
+
+        for cor in range(num_cores):
+            if eh_cor_segura(vertice, cor):
+                cores[vertice] = cor
+                if colorir_util(vertice + 1, num_cores):
+                    return True
+                cores[vertice] = -1
+
+    num_cores = num_vertices
+    if not colorir_util(0, num_cores):
+        return "Não é possível colorir o grafo com as cores especificadas."
+
+    resultado = {}
+    for vertice, cor in enumerate(cores):
+        resultado[vertice] = cor
+
+    return resultado
+
+
 def generate_score(v1: Vertice, v2: Vertice):
     points = 0
 
-    if v1['course'] == v2['course'] and \
-        v1['code'] == v2['code']:
+    if v1["course"] == v2["course"] and v1["code"] == v2["code"]:
         return points
 
-    if v1['teacher'] == v2['teacher']:
+    if v1["teacher"] == v2["teacher"]:
         points += 1
+        return 1
 
-    if v1['semester'] == v2['semester'] and \
-        v1['course'] == v2['course']:
+    if v1["semester"] == v2["semester"] and v1["course"] == v2["course"]:
         points += 2
+        return 1
 
     return points
 
 
 if __name__ == "__main__":
-    cenario1 = read_json_data('./cenario1.json')
+    cenario1 = read_json_data("./cenario1.json")
 
     matrix = generate_adj_matrix(cenario1)
 
-    print(matrix)
+    cores = coloracao_por_matriz_adjacencia(matrix)
+
+    print(cores)
