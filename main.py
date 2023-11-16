a8a1221d1b680fd2147e19b2135b10106716891d
leandrobalta <leandrobraga.job@gmail.com>
2023-11-16 19:03:50 -0300

    added jv incredible code and my double for

--- main.py
+++ main.py
@@ -1,49 +1,5 @@
 import json
 
-
-""" class Conflito:
-    codigo: str
-    type: str | int """
-
-class Conflito:
-    def __init__(self, codigo, type):
-        self.codigo: str = codigo
-        self.type: str | int = type
-
-
-class Disciplina:
-    curso: str
-    semestre: str
-    codigo: str
-    nome: str
-    ch: str
-    prof: str
-    conflitos: list[Conflito]
-
-""" class Disciplina:
-    def __init__(self, curso, semestre, codigo, nome, ch, prof):
-        self.curso: str = curso
-        self.semestre: str = semestre
-        self.codigo: str = codigo
-        self.nome: str = nome
-        self.ch: str = ch
-        self.prof: str = prof
-
-        self.conflitos: list[Conflito] = [] """
-
-
-class Vertice:
-    def __init__(self, curso, semestre, codigo, disciplina, ch, prof):
-        self.curso: str = curso
-        self.semestre: str = semestre
-        self.codigo: str = codigo
-        self.disciplina: str = disciplina
-        self.ch: str = ch
-        self.prof: str = prof
-
-        self.conflitos: list[Conflito] = []
-
-
 def get_teacher(row):
     teacher = ""
     for col in df.columns[6:]:
@@ -70,6 +26,32 @@ def get_cenario(path):
 
     print(items)
 
+class Conflito:
+    def __init__(self, codigo, type):
+        self.codigo: str = codigo
+        self.type: str | int = type
+
+
+class Disciplina:
+    curso: str
+    semestre: str
+    codigo: str
+    nome: str
+    ch: str
+    prof: str
+    conflitos: list[Conflito]
+
+class Vertice:
+    def __init__(self, curso, semestre, codigo, disciplina, ch, prof):
+        self.curso: str = curso
+        self.semestre: str = semestre
+        self.codigo: str = codigo
+        self.disciplina: str = disciplina
+        self.ch: str = ch
+        self.prof: str = prof
+
+        self.conflitos: list[Conflito] = []
+
 
 def read_json_data(filename):
     with open(filename, "r") as file:
@@ -90,14 +72,18 @@ def generate_adj_matrix(cenario):
 
     return matrix
 
+def get_color():
+    pass
+
 
 def coloracao_por_matriz_adjacencia(matriz_adjacencia):
     num_vertices = len(matriz_adjacencia)
-    cores = [-1] * num_vertices  # Inicializa todas as cores como -1
+    schedules = [-1] * num_vertices  # Inicializa todas as cores como -1
+    num_cores = 74
 
     def eh_cor_segura(vertice, cor):
         for i in range(num_vertices):
-            if matriz_adjacencia[vertice][i] == 1 and cores[i] == cor:
+            if matriz_adjacencia[vertice][i] == 1 and schedules[i] == cor:
                 return False
         return True
 
@@ -105,19 +91,19 @@ def coloracao_por_matriz_adjacencia(matriz_adjacencia):
         if vertice == num_vertices:
             return True
 
+        # aqui tem que aplicar a funao que retorna exatamente os horarios que podem ser usados
         for cor in range(num_cores):
             if eh_cor_segura(vertice, cor):
-                cores[vertice] = cor
+                schedules[vertice] = cor
                 if colorir_util(vertice + 1, num_cores):
                     return True
-                cores[vertice] = -1
+                schedules[vertice] = -1
 
-    num_cores = num_vertices
     if not colorir_util(0, num_cores):
         return "Não é possível colorir o grafo com as cores especificadas."
 
     resultado = {}
-    for vertice, cor in enumerate(cores):
+    for vertice, cor in enumerate(schedules):
         resultado[vertice] = cor
 
     return resultado
@@ -164,15 +150,76 @@ def graphColoring(adjacencyMatrix, numColors):
     
     return colors
 
+#---------------------------------------------------------------------
+
+def available(graph, index, horarios, horarioEscolhido):
+
+    # Restrição de horários sequenciais
+    count = 0
+    for i in range(graph[index]['ch']):
+        if horarios[horarioEscolhido - i]:
+            # verifica se ja possui a disciplina no horario anterior
+            for j in range(len(horarios[horarioEscolhido - i])):
+                if graph[index]['code'] == horarios[horarioEscolhido - i][j]['code'] and graph[index]['course'] == horarios[horarioEscolhido - i][j]['course']:
+                    count += 1
+                    break
+    if count >= 2 and graph[index]['ch'] >= 4:
+        return False
+
+
+    # Restricao de professor
+    for j in range(len(horarios[horarioEscolhido])):
+        if graph[index]['teacher'] == horarios[horarioEscolhido][j]['teacher']:
+            return False
+
+    # Restricao de semestre e curso
+    for j in range(len(horarios[horarioEscolhido])):
+        if graph[index]['course'] == horarios[horarioEscolhido][j]['course'] and graph[index]['semester'] == horarios[horarioEscolhido][j]['semester']:
+            return False
+
+    
+    return True
+
+
+def allocateSinCourses(graph, index, horarios):
+    for i in range(int(graph[index]['ch'])):
+        for j in range(len(horarios)):
+            if (j > 10 and j < 15) or (j > 24 and j < 29) or (j > 38 and j < 43) or (j > 52 and j < 57) or (j > 66 and j < 71):
+                if available(graph, index, horarios, j):
+                    horarios[j].append({ 'name': graph[index]['name'],'code': graph[index]['code'] ,'semester': graph[index]['semester'], 'course': graph[index]['course'], 'teacher': graph[index]['teacher'], 'ch': graph[index]['ch'], 'horario': j})
+                    break
+
+    return horarios
+
+
+def allocateOtherCourses(graph, index, horarios):
+    for i in range(int(graph[index]['ch'])):
+        for j in range(len(horarios)):
+            if available(graph, index, horarios, j):
+                horarios[j].append({ 'name': graph[index]['name'],'code': graph[index]['code'] ,'semester': graph[index]['semester'], 'course': graph[index]['course'], 'teacher': graph[index]['teacher'], 'ch': graph[index]['ch'], 'horario': j})
+                break
+
+    return horarios
+
+
+def getHorarios(array, graph):
+    horarios = [[] for i in range(74)]
+    
+    for i in range(len(array)):
+        if graph[i]['course'] == 'SIN':
+            horarios = allocateSinCourses(graph, i, horarios)
+    
+    for i in range(len(array)):
+        if graph[i]['course'] != 'SIN':
+            horarios = allocateOtherCourses(graph, i, horarios)
+    
+    return horarios
 
 if __name__ == "__main__":
     cenario1 = read_json_data("./cenario1.json")
 
     matrix = generate_adj_matrix(cenario1)
 
-    cores = graphColoring(matrix, 74);
+    cores = graphColoring(matrix, 74)
 
-    for i in range(len(cenario1)):
-        print(f"------------\n\n    code: {cenario1[i]['code']} \n    Teacher: {cenario1[i]['teacher']} \n    Semester: {cenario1[i]['semester']} \n    Result: {cores[i]}")
-
-    print(cores)
+    print(getHorarios(cores, cenario1))
